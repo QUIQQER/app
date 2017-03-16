7c6dae49c45ba670a0cefbd49e9d57ea2edd6442
Jan Wennrich <jan@pcsg.de>
2017-03-16 10:29:04 +0100

    feat: Display first page from menu on startup

--- build/src/pages/home/home.ts
+++ build/src/pages/home/home.ts
@@ -2,44 +2,43 @@ import {Component} from '@angular/core';
 import {NavParams, ModalController} from "ionic-angular";
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 import {ImprintPage} from "../../modals/imprint/imprint";
+import {pages} from "../../app/pages";
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
-export class HomePage
-{
-    private defaultUrl: string = 'http://placehold.it/200x200';
+export class HomePage {
     private url: SafeResourceUrl;
-
     private title: String = 'Home';
 
     constructor(private params: NavParams,
                 private sanitizer: DomSanitizer,
-                private modalCtrl: ModalController)
-    {
-        let url   = params.get('url');
+                private modalCtrl: ModalController) {
+        let url = params.get('url');
         let title = params.get('title');
+
+        let Page = pages[0];
 
         // If opened with URL param we have to tell Angular it's save since it's used for iframe src
         if (typeof url == 'undefined') {
-            // If no URL provided (e.g.on startup) use the default url
-            this.url = sanitizer.bypassSecurityTrustResourceUrl(this.defaultUrl);
+            // If no URL provided (e.g.on startup) use the first url from menu
+            this.url = sanitizer.bypassSecurityTrustResourceUrl(Page.url);
+
         } else {
-            this.url = sanitizer.bypassSecurityTrustResourceUrl(url+'?app=1');
+            this.url = sanitizer.bypassSecurityTrustResourceUrl(url + '?app=1');
         }
 
-        // If title provided use it
         if (typeof title != 'undefined') {
-            this.title = title;
+            // If no title provided (e.g.on startup) use the first title from menu
+            this.title = Page.title;
         }
     }
 
     /**
      * Opens the imprint modal
      */
-    public showImprint()
-    {
+    public showImprint() {
         this.modalCtrl.create(ImprintPage).present();
     }
 }
