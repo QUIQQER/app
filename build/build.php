a77239cff181ab580b01d7133ada9f90ef3fac46
Jan Wennrich <jan@pcsg.de>
2017-05-02 11:04:39 +0200

    feat: Run icon&splash generation interactively

--- build/build.php
+++ build/build.php
@@ -185,8 +185,19 @@ if (!empty($logo) && $generateIcon) {
 
     echo "\nGeneriere Icons...\n";
 
+    $cmd = 'ionic cordova resources --icon';
+
     // Execute Ionic icon generation command
-    echo shell_exec('ionic cordova resources --icon');
+    $result = liveExecuteCommand($cmd);
+
+    if ($result['exit_status'] == 1) {
+        error(
+            'Something went wrong generating the icons.' . PHP_EOL
+            . "Try again later or try running '{$cmd}' manually" . PHP_EOL
+            . "Further information:" . PHP_EOL
+            . $result['output']
+        );
+    }
 }
 
 // If Splash URL and no Flag set, generate Splash
@@ -196,8 +207,19 @@ if (!empty($splash) && $generateSplash) {
 
     echo "\nGeneriere Splashscreens...\n";
 
+    $cmd = 'ionic cordova resources --splash';
+
     // Execute Ionic splash generation command
-    echo shell_exec('ionic cordova resources --splash');
+    $result = liveExecuteCommand($cmd);
+
+    if ($result['exit_status'] == 1) {
+        error(
+            'Something went wrong generating the splashscreens.' . PHP_EOL
+            . "Try again later or try running '{$cmd}' manually" . PHP_EOL
+            . "Further information:" . PHP_EOL
+            . $result['output']
+        );
+    }
 }
 
 
@@ -242,6 +264,44 @@ $xmlAuthorAttributes->href  = $author->website;
 $xmlConfig->saveXML('config.xml');
 
 echo "\nBuild completed\n";
+
+
+/**
+ * Execute the given command by displaying console output live to the user.
+ * From Amith via Stackoverflow (http://stackoverflow.com/a/32664523/3002417)
+ * @param  string $cmd :  command to be executed
+ * @return array   exit_status  :  exit status of the executed command
+ *                 output       :  console output of the executed command
+ */
+function liveExecuteCommand($cmd)
+{
+    // end all output buffers if any
+    while (@ ob_end_flush()) {
+    }
+
+    $proc = popen("$cmd 2>&1 ; echo Exit status : $?", 'r');
+
+    $live_output     = "";
+    $complete_output = "";
+
+    while (!feof($proc)) {
+        $live_output     = fread($proc, 4096);
+        $complete_output = $complete_output . $live_output;
+        echo "$live_output";
+        @ flush();
+    }
+
+    pclose($proc);
+
+    // get exit status
+    preg_match('/[0-9]+$/', $complete_output, $matches);
+
+    // return exit status and intended output
+    return array(
+        'exit_status' => intval($matches[0]),
+        'output'      => str_replace("Exit status : " . $matches[0], '', $complete_output)
+    );
+}
 
 /**
  * Outputs an error message (colored red)
