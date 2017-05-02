cd70a29653c1f5258f156429f13f3c77d5f222fc
Jan Wennrich <jan@pcsg.de>
2017-05-02 13:03:28 +0200

    feat: Project parameter & include API URL

--- bin/download.php
+++ bin/download.php
@@ -9,15 +9,43 @@ if (!QUI::getUserBySession()->isSU()) {
     exit;
 }
 
+try {
+    $projectName = $_GET['project'];
+    $Project     = QUI::getProject($projectName);
+} catch (\Exception $exception) {
+    exit;
+}
+
+$projectName = $Project->getName();
+
 // Folder to store our ZIP
 $tempFolderPath = QUI::getTemp()->createFolder('app');
 
+$zipName = "app-{$projectName}.zip";
+$zipPath = $tempFolderPath . $zipName;
+
+// Delete older zip, since the zip isn't overwritten
+if (file_exists($zipPath)) {
+    unlink($zipPath);
+}
+
 // ZIP the build/ folder
-\QUI\Archiver\Zip::zip($packagesDir . "quiqqer/app/build", $tempFolderPath . "app.zip");
+\QUI\Archiver\Zip::zip($packagesDir . "quiqqer/app/build", $zipPath);
+
+
+// Add config file for API URL
+$Zip = new ZipArchive();
+$Zip->open($zipPath, ZipArchive::CREATE);
+
+$apiUrl  = QUI\REST\Server::getInstance()->getAddress() . "quiqqer/app/structure/$projectName/de";
+$content = "api_url=\"$apiUrl\"";
+$Zip->addFromString('config.ini', $content);
+
+$Zip->close();
 
 // Return file download
 header("Content-Type: application/zip");
-header("Content-Disposition: attachment; filename=\"app.zip\"");
-readfile($tempFolderPath . "app.zip");
+header("Content-Disposition: attachment; filename=\"$zipName\"");
+readfile($zipPath);
 
 exit;
