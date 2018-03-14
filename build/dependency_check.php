f0030c3cdf403f604be7591ee7915f71d500117b
Jan Wennrich <jan@pcsg.de>
2018-03-14 15:19:20 +0100

    docs: added docs

--- build/dependency_check.php
+++ build/dependency_check.php
@@ -6,8 +6,15 @@ const TEXT_RED   = "\033[31m";
 const TEXT_GREEN = "\033[32m";
 const TEXT_RESET = "\033[0m";
 
+/**
+ * Prints out all dependencies.
+ * Dependency is colored green when it's present, otherwise it's colored red
+ *
+ * @return bool
+ */
 function checkDependencies()
 {
+    // [Name of the dependency => function that checks if it's present]
     $dependencies = array(
         'Node'          => 'isNodeInstalled',
         'php-xml'       => 'isPhpXMLInstalled',
@@ -22,6 +29,7 @@ function checkDependencies()
     foreach ($dependencies as $name => $checkFunction) {
         echo TEXT_GREEN;
 
+        // If dependency is not present...
         if (!$checkFunction()) {
             echo TEXT_RED;
             $allDependenciesPresent = false;
--- build/dependency_check_functions.php
+++ build/dependency_check_functions.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Returns if Node is installed
+ * Interprets the output of 'node -v' and it's return code
+ *
+ * @return bool
+ */
 function isNodeInstalled()
 {
     exec("node -v", $output, $returnCode);
@@ -16,14 +22,27 @@ function isNodeInstalled()
 }
 
 
+/**
+ * Returns if php-xml extension is installed
+ * Checks if 'xml' is in the loaded php-extensions
+ *
+ * @return bool
+ */
 function isPhpXMLInstalled()
 {
     return in_array('xml', get_loaded_extensions());
 }
 
 
+/**
+ * Checks if Oracle JDK is installed
+ * Interprets the return code of 'javac -version'
+ *
+ * @return bool
+ */
 function isOracleJdkInstalled()
 {
+    // Route output of the command to /dev/null since javac behaves weird with exec
     exec("javac -version 2> /dev/null", $output, $returnCode);
 
     if ($returnCode != 0) {
@@ -34,6 +53,11 @@ function isOracleJdkInstalled()
 }
 
 
+/**
+ * Checks if the JAVA_HOME environment variable is set
+ *
+ * @return bool
+ */
 function isJavaHomeSet()
 {
     if (!getenv('JAVA_HOME')) {
@@ -44,6 +68,12 @@ function isJavaHomeSet()
 }
 
 
+/**
+ * Checks if Ionic Framework is installed (globally)
+ * Interprets the output and return code of 'ionic -v'
+ *
+ * @return bool
+ */
 function isIonicInstalled()
 {
     exec('ionic -v', $output, $returnCode);
@@ -61,6 +91,12 @@ function isIonicInstalled()
 }
 
 
+/**
+ * Checks if Cordova is installed (globally)
+ * Interprets the output and return code of 'cordova -v'
+ *
+ * @return bool
+ */
 function isCordovaInstalled()
 {
     exec('cordova -v', $output, $returnCode);
